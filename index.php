3711739c2a490f5d9728950bff96cc5f2613f081
guillermosg28 <guillermo_sg_12@hotmail.com>
2015-09-09 17:25:07 -0500

    nuevo
    nuevo

---
+++ index.php
@@ -0,0 +1,164 @@
+
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->  
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->  
+<!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->  
+<head>
+    <title>Facultad de Ingenieria de Zootecnia | UNPRG</title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">    
+    <link rel="shortcut icon" href="favicon.ico">  
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'>   
+    <!-- Global CSS -->
+    <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="plugins/bootstrap/css/font-awesome.css"> 
+    <!-- Plugins CSS -->    
+    <link rel="stylesheet" href="plugins/flexslider/flexslider.css">
+    <link rel="stylesheet" href="plugins/pretty-photo/css/prettyPhoto.css"> 
+    <!-- Diseño CSS -->  
+    <link id="theme-style" rel="stylesheet" href="css/styles.css">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body class="home-page">
+
+    <div class="wrapper">
+        <!-- ******HEADER****** --> 
+        <header class="header">  
+            <div class="top-bar">
+                <div class="container">              
+                    <ul class="social-icons col-md-6 col-sm-6 col-xs-12 hidden-xs">
+                        <li><a href="#" ><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#" ><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#" ><i class="fa fa-youtube"></i></a></li>            
+                    </ul><!--//social-icons-->
+                    <form class="pull-right search-form" role="search">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Buscar...">
+                        </div>
+                        <button type="submit" class="btn btn-theme">Buscar</button>
+                    </form>         
+                </div>      
+            </div><!--//to-bar-->
+            <div class="header-main container">
+                <h1 class="logo col-md-4 col-sm-6">
+                    <a href="#"><img id="logo" src="imagenes/logo.png" alt="Logo"></a>
+                </h1><!--//logo-->           
+                <div class="info col-md-8 col-sm-6">
+                    <ul class="menu-top navbar-right">
+                        <li class="divider"><a href="#"><i class="fa fa-phone"></i> Directorio telefónico</a></li>
+                        <li class="divider"><a href="#"><i class="fa fa-envelope"></i> Correo Institucional</a></li>
+                        <li><a href="#"><i class="fa fa-building-o"></i> Actas</a></li>
+                    </ul><!--//menu-top-->
+                </div><!--//info-->
+            </div><!--//header-main-->
+        </header><!--//header-->
+
+
+        <!-- ******NAV****** -->
+        <nav class="main-nav" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-collapse">
+                        <span class="sr-only">Menu</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button><!--//nav-toggle-->
+                </div><!--//navbar-header-->            
+                <div class="navbar-collapse collapse" id="navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active nav-item"><a href="#">Inicio</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="0" data-close-others="false" href="#">Autoridades <i class="fa fa-angle-down"></i></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Mensaje del Rector</a></li>
+                                <li><a href="#">Alta Direccion</a></li>   
+                                <li><a href="#">Directivos</a></li>
+                                <li><a href="#">Decanos</a></li>
+                                <li><a href="#">Jefes Oficinas Centrales</a></li>
+                                <li><a href="#">Jefes Oficinas</a></li>
+                                <li><a href="#">Asamblea Estatutaria</a></li>          
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="0" data-close-others="false" href="#">Facultades <i class="fa fa-angle-down"></i></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Fac. de Ing. Agronoma</a></li>
+                                <li><a href="#">Fac. de Cc. Biológicas</a></li>   
+                                <li><a href="#">Fac. de Cc. Econ., Adm. y Cont.</a></li>
+                                <li><a href="#">Fac. de Cc. Físicas y Mat.</a></li>
+                                <li><a href="#">Fac. de Cc. HH. SS y Educación</a></li>
+                                <li><a href="#">Fac. de Derecho y Cc. Políticas</a></li>
+                                <li><a href="#">Fac. de Enfermería</a></li> 
+                                <li><a href="#">Fac. de Ing. Agrícola</a></li> 
+                                <li><a href="#">Fac. de Ing. Mecánica y Eléc.</a></li> 
+                                <li><a href="#">Fac. de Ing. Zootecnia</a></li> 
+                                <li><a href="#">Fac. de Medicina Humana</a></li> 
+                                <li><a href="#">Fac. de Medicina Veterinaria</a></li> 
+                                <li><a href="#">Fac. Ing. Civil, Sist. y Arq.</a></li> 
+                                <li><a href="#">Fac. Quimica e Ind. Alim.</a></li>         
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="0" data-close-others="false" href="#">Direcciones univ. <i class="fa fa-angle-down"></i></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Rectorado</a></li>
+                                <li><a href="#">Vicerrectorado de Investigación</a></li>   
+                                <li><a href="#">Vicerrectorado Académico</a></li>
+                                <li><a href="#">Órgano de Control Institucional</a></li>         
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="0" data-close-others="false" href="#">Directorio <i class="fa fa-angle-down"></i></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Central Telefónica</a></li>
+                                <li><a href="#">Directorio Facultades</a></li>   
+                                <li><a href="#">Directorio Oficinas</a></li>       
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="0" data-close-others="false" href="#">Documentos Oficiales <i class="fa fa-angle-down"></i></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">TUPA 2014</a></li>
+                                <li><a href="#">Resoluciones Rectorales</a></li>   
+                                <li><a href="#">Convenios</a></li>
+                                <li><a href="#">Nuevo Estatuto 2014</a></li>         
+                            </ul>
+                        </li>
+                        <li class="nav-item"><a href="contact.html">RADIO UNIVERSITARIA</a></li>
+                    </ul><!--//nav-->
+                </div><!--//navabr-collapse-->
+            </div><!--//container-->
+        </nav><!--//main-nav-->
+
+
+
+    </div> <!-- fin wrapper -->
+
+
+
+
+
+    <!-- Javascript -->          
+    <script type="text/javascript" src="plugins/jquery-1.11.2.min.js"></script>
+    <script type="text/javascript" src="plugins/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="plugins/bootstrap/js/bootstrap.min.js"></script> 
+    <script type="text/javascript" src="plugins/bootstrap-hover-dropdown.min.js"></script> 
+    <script type="text/javascript" src="plugins/back-to-top.js"></script>
+    <script type="text/javascript" src="plugins/jquery-placeholder/jquery.placeholder.js"></script>
+    <script type="text/javascript" src="plugins/pretty-photo/js/jquery.prettyPhoto.js"></script>
+    <script type="text/javascript" src="plugins/flexslider/jquery.flexslider-min.js"></script>
+    <script type="text/javascript" src="plugins/jflickrfeed/jflickrfeed.min.js"></script> 
+    <script type="text/javascript" src="js/main.js"></script>  
+
+</body>
+</html> 
